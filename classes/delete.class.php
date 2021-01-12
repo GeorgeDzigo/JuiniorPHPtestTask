85408c8e38f764fd171c0bd465c7dc5bd3a643f6
GeorgeDzigo <mr.georgegamertv@gmail.com>
2021-01-12 23:07:49 +0200

    Little Update

--- classes/delete.class.php
+++ classes/delete.class.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../db.php';
-
+require_once '../id.php';
 class Delete extends DB {
       public function delete($a) {
             $sql = $this->connect()->prepare("DELETE FROM products WHERE unique_id = :id");
